af6ef6225ac50e52df63883c75d32d920f3cedf6
rkun <tren111213@gmail.com>
2016-09-19 14:28:29 +0900

    getやpostはhtmlとjsを別ドメインに置いたら chromeにクロスドメインだめよって弾かれるので。。。 ゲームごとherokuにホスティングさせる形にします。

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var http = require("http");
 var url = require("url");
 var qs = require("querystring");
-
+var fs = require("fs");
 var server = http.createServer(function (req,res) {
-    console.log("getConection.");
-    console.log(req.body);
-    var paras = url.parse(req.url,true);
-    console.log(paras.query);
+        console.log(req.url);
+        console.log("getConection.");
+        console.log(req.body);
+        var paras = url.parse(req.url,true);
+        console.log(paras.query);
+
+
+        res.setHeader("Content-Type", "text/html");
+        res.end(fs.readFileSync("./htmls"+req.url));
 });
 server.listen(3000);
